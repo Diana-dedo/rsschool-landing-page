80e7a2e691c2fd046905d95295c5d1af7c264a76
Diana_dedo <d.varnauskaya@gmail.com>
2026-09-26 16:33:44 +0300

    feat: make a burger

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
     document.body.classList.add('dark-theme');
 }
+
+const burgerButton = document.querySelector('.menu_button');
+const headerNavigation = document.querySelector('.header_nav');
+const allMenuLinks = document.querySelectorAll('.header_list .link');
+
+function toggleMobileMenu() {
+    headerNavigation.classList.toggle('is-open');
+    document.body.classList.toggle('no-scroll');
+    burgerButton.classList.toggle('is-open');
+}
+
+function closeMobileMenu() {
+    headerNavigation.classList.remove('is-open');
+    document.body.classList.remove('no-scroll');
+    burgerButton.classList.remove('is-open');
+}
+
+burgerButton.addEventListener('click', toggleMobileMenu); 
+    allMenuLinks.forEach(link => {
+        link.addEventListener('click', closeMobileMenu);
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 769) {
+        closeMobileMenu();
+    }
+}); 
